ecd0cb0b64a51aa98c0854205d5a046abdb51287
Allandt Bik-Elliott <allandt@gmail.com>
2023-11-17 13:12:19 +0000

    use a struct in the template

--- main.go
+++ main.go
@@ -9,19 +9,23 @@ import(
 // create template container and load all templates into it
 var tpl *template.Template
 
+type book struct {
+	Title	string
+	Author	string
+}
+
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
 }
 
 func main() {
-	books := map[string]string {
-		"Harry Potter": "J K Rowling",
-		"The Bible": "God",
-		"Neuromancer": "William Gibson",
+	bible := book{
+		Title: "The Bible",
+		Author: "God",
 	}
 
 	// execute to stdout
-	err := tpl.ExecuteTemplate(os.Stdout, "index.gohtml", books)
+	err := tpl.ExecuteTemplate(os.Stdout, "index.gohtml", bible)
 	if err != nil {
 		log.Fatalln(err)
 	}
